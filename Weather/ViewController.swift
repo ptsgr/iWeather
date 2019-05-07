22c4eb23ab888f4feff56a735748add05ea0aa8d
Egor Pats <egor.pats@gmail.com>
2019-05-08 02:13:46 +0300

    Add switch mod and weather image.

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -11,16 +11,74 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var SwitchMod: UISwitch!
+    @IBOutlet weak var switchLabel: UILabel!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     
+    var whiteColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    var blackColor = #colorLiteral(red: 0.1215686275, green: 0.1294117647, blue: 0.1411764706, alpha: 1)
+    var blackForLabels = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    var whiteForLabels = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    
+    var swithIs: Int!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        checkSwitchMod()
         searchBar.delegate = self
     }
-
+    
+    @IBAction func SwitchOn(_ sender: Any) {
+        if SwitchMod.isOn == true {
+            
+            
+            mainView.backgroundColor = blackColor
+            cityLabel.textColor = whiteForLabels
+            tempLabel.textColor = whiteForLabels
+            switchLabel.textColor = whiteForLabels
+            searchBar.barStyle = .black
+            
+            swithIs = 1
+            UserDefaults.standard.set(swithIs, forKey: "mode")
+        } else {
+            
+            mainView.backgroundColor = whiteColor
+            cityLabel.textColor = blackForLabels
+            tempLabel.textColor = blackForLabels
+            switchLabel.textColor = blackForLabels
+            searchBar.barStyle = .default
+            
+            swithIs = 2
+            UserDefaults.standard.set(swithIs, forKey: "mode")
+        }
+       
+    }
+    
+    func checkSwitchMod() {
+        if UserDefaults.standard.integer(forKey: "mode") == 1 {
+            mainView.backgroundColor = blackColor
+            cityLabel.textColor = whiteForLabels
+            tempLabel.textColor = whiteForLabels
+            switchLabel.textColor = whiteForLabels
+            searchBar.barStyle = .black
+            SwitchMod.isOn = true
+        } else {
+            mainView.backgroundColor = whiteColor
+            cityLabel.textColor = blackForLabels
+            tempLabel.textColor = blackForLabels
+            switchLabel.textColor = blackForLabels
+            searchBar.barStyle = .default
+            SwitchMod.isOn = false
+        }
+    }
+    
+    
 
 }
 
@@ -31,6 +89,8 @@ extension ViewController: UISearchBarDelegate {
         
         var locationName: String?
         var currentTemp: Double?
+        var urlForImage: String?
+        var dataImage: Data?
         var errorHasOccured: Bool = false
         
         let urlSearch =     "https://api.apixu.com/v1/current.json?key=cb09fdee98dc48b6b88201642190605&q=\(searchBar.text!.replacingOccurrences(of: " ", with: "%20"))"
@@ -49,14 +109,18 @@ extension ViewController: UISearchBarDelegate {
                 
                 if let location = json["location"] {
                     locationName = location["name"] as? String
-                    
-                 
-                    
                 }
+                
+                if let imageAPIcurrent = json["current"]{
+                    if let imageAPI = imageAPIcurrent["condition"] as? [String : AnyObject]{
+                        urlForImage  = "https:\(imageAPI["icon"] as? String ?? "//avtodok02.ru/wp-content/uploads/2018/09/01-Location.png")"
+                        let urlImage = URL(string: urlForImage!)
+                        dataImage = try? Data(contentsOf: urlImage!)
+                    }
+                }
+                
                 if let current = json["current"] {
                     currentTemp = current["temp_c"] as? Double
-                    
-                    
                 }
                 DispatchQueue.main.async {
                     if errorHasOccured {
@@ -64,6 +128,7 @@ extension ViewController: UISearchBarDelegate {
                         self?.tempLabel.isHidden = true
                         
                     } else {
+                        self?.weatherImage.image = UIImage(data: dataImage!)
                         self?.cityLabel.text = locationName
                         self?.tempLabel.text = "\(currentTemp!)"
                         self?.tempLabel.isHidden = false
