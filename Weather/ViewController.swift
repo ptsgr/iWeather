545b4c54165e289124b8e6a9fa32f7eaebc569ca
Egor Pats <egor.pats@gmail.com>
2019-05-07 07:08:26 +0300

    Added work with API and url session

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -10,11 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchBar.delegate = self
     }
 
 
 }
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        
+        var locationName: String?
+        var currentTemp: Double?
+        
+        let urlSearch =     "https://api.apixu.com/v1/current.json?key=cb09fdee98dc48b6b88201642190605&q=\(searchBar.text!)"
+        let url = URL(string: urlSearch)
+        let task = URLSession.shared.dataTask(with: url!) {[weak self] (data, response, error) in
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                    as! [String : AnyObject]
+                print("json init!")
+                
+                
+                if let location = json["location"] {
+                    locationName = location["name"] as? String
+                    
+                 
+                    
+                }
+                if let current = json["current"] {
+                    currentTemp = current["temp_c"] as? Double
+                    
+                    
+                }
+                DispatchQueue.main.async {
+                    self?.cityLabel.text = locationName
+                    self?.tempLabel.text = "\(currentTemp!)"
+                }
+                
+            } catch let jsonError {
+                print("kek")
+                print(jsonError)
+            }
+        }
+        task.resume()
+        
+        
+     /*   */
+        
+    }
+}
